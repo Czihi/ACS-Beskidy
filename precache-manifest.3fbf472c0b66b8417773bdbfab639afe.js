a798656611826f1925dce67e80e49e2a36f8500a
Czihi <boslak@student.agh.edu.pl>
2020-07-31 19:08:10 +0200

    Updates

---
+++ precache-manifest.3fbf472c0b66b8417773bdbfab639afe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d190ec59b2fa235430233f6f848a9fc",
+    "url": "/ACS-Beskidy/static/media/background.6d190ec5.png"
+  },
+  {
+    "revision": "01b48088179b19f3095b",
+    "url": "/ACS-Beskidy/static/js/runtime~main.d8c1c124.js"
+  },
+  {
+    "revision": "9ffacac464918257854b",
+    "url": "/ACS-Beskidy/static/js/main.442fb41d.chunk.js"
+  },
+  {
+    "revision": "0843c9631369002c228e",
+    "url": "/ACS-Beskidy/static/js/2.26a1118d.chunk.js"
+  },
+  {
+    "revision": "9ffacac464918257854b",
+    "url": "/ACS-Beskidy/static/css/main.3ef2abf6.chunk.css"
+  },
+  {
+    "revision": "0843c9631369002c228e",
+    "url": "/ACS-Beskidy/static/css/2.5c743e86.chunk.css"
+  },
+  {
+    "revision": "f53ad472e7d41f37f7c228f8b6588695",
+    "url": "/ACS-Beskidy/index.html"
+  }
+];
